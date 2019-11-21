1e6ad49e06e50a3d7a57634270b7e7ce97a8a7ea
tma <tma@S.local>
2019-11-21 17:19:28 +0300

    Openvino l2

--- samples/practice2_template.py
+++ samples/practice2_template.py
@@ -12,64 +12,59 @@ sys.path.append('../src')
 from ie_detector import InferenceEngineDetector
 def build_argparse():
     parser = argparse.ArgumentParser()
+    parser.add_argument("-img", type=bool, help="Work with image", required=False)
+    parser.add_argument("-vid", type=str, help="Work with video", required=False)
     parser.add_argument("-imgpath", type=str, help="Path to image", required=False)
     parser.add_argument("-lw", type=str, help="Width of line", required=False, default = 1)
     parser.add_argument("-vidpath", type=str, help="Path to video", required=False)
+    args, leftovers = parser.parse_known_args()
     return parser
+def cycle(IE,image,width,height,classes,lw):
 
+        result,fps = IE.detect(image)
+        for i in result:
+            for j in i:
+                result = j[~(j == 0).all(1)]
+        for i in range(len(result)):
+            data = result[i, :]
+            if (data[2] > 0.5):
+                name = int(data[1])
+                name = classes[name]
+                xmin = int(data[3] * width)
+                ymin = int(data[4] * height)
+                xmax = int(data[5] * width)
+                ymax = int(data[6] * height)
+                image = IE.draw_detection(image, xmin, ymin, xmax, ymax,lw, name,fps)
+        return image
 def main():
     args = build_argparse().parse_args()
-    #image = cv2.imread(args.path)
-    #height = image.shape[0]
-    #width = image.shape[1]
-    cap = cv2.VideoCapture(args.vidpath)
-    if (cap.isOpened() == False):
-        print("Error opening video stream or file")
-
-    # Read until video is completed
-    while (cap.isOpened()):
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        if ret == True:
-            # Display the resulting frame
-            IE = InferenceEngineDetector()
-            result = IE.detect(frame)
-            for i in result:
-                for j in i:
-                    result = j[~(j == 0).all(1)]
-            number_iter = len(result)
-            classes = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair",
-                       "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
-            width = cap.get(3)
-            height = cap.get(4)
-            for i in result:
-                print(i)
+    IE = InferenceEngineDetector()
+    classes = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair",
+               "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train",
+               "tvmonitor"]
+    if args.img:
+        image = cv2.imread(args.imgpath)
+        height = image.shape[0]
+        width = image.shape[1]
+        image = cycle(IE,image,width,height,classes,args.lw)
+        cv2.imshow("Image", image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    elif args.vid:
+        cap = cv2.VideoCapture(args.vidpath)
+        while True:
+            ret, frame = cap.read()
+            if ret == True:
+                width = cap.get(3)
+                height = cap.get(4)
+                frame= cycle(IE,frame, width, height, classes,args.lw)
+                cv2.imshow("Video", frame)
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                        break
+            else:
                 break
-                name = int(result[0][1])
-                name = classes[name]
-                xmin = int(result[0][3] * width)
-                ymin = int(result[0][4] * height)
-                xmax = int(result[0][5] * width)
-                ymax = int(result[0][6] * height)
-                img = IE.draw_detection(frame, xmin, ymin, xmax, ymax, args.lw, name)
-            #cv2.imshow("draw", img)
-            # Press Q on keyboard to  exit
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
-
-        # Break the loop
-        else:
-            break
-
-    # When everything done, release the video capture object
-    cap.release()
-
-    # Closes all the frames
-    cv2.destroyAllWindows()
-
-    #self.number_iter = 25
-
-    return 
-
+        cap.release()
+        cv2.destroyAllWindows()
+    return
 if __name__ == '__main__':
     sys.exit(main()) 
--- src/ie_detector.py
+++ src/ie_detector.py
@@ -1,19 +1,18 @@
-"""
-
-Inference engine detector
- 
-"""
 import cv2
 import numpy as np
 import openvino
 from openvino.inference_engine import IENetwork, IECore
 from time import time
+import logging as log
+import sys
 class InferenceEngineDetector:
     def __init__(self, weightsPath = None, configPath = None,
                  device = 'CPU', extension = None):
-        configPath = 'C:/UNN_HPC_SCHOOL_2019_ML/model\public/mobilenet-ssd\FP16/mobilenet-ssd.xml'
+        configPath = 'C:/Calibration/model/public/mobilenet-ssd/FP32/mobilenet-ssd_i8.xml'
+        #configPath = 'C:/Calibration/model/public/ssd300/FP32/ssd300.xml'
         cpu_extension = "C:/Program Files (x86)/IntelSWTools/openvino/inference_engine/bin/intel64/Release/cpu_extension_avx2.dll"
-        weightsPath = "C:/UNN_HPC_SCHOOL_2019_ML/model/public/mobilenet-ssd/FP16/mobilenet-ssd.bin"
+        weightsPath = "C:/Calibration/model/public/mobilenet-ssd/FP32/mobilenet-ssd_i8.bin"
+        #weightsPath = "C:/Calibration/model/public/ssd300/FP32/ssd300.bin"
         self.ie = IECore()
         if cpu_extension:
             self.ie.add_extension(cpu_extension, 'CPU')
@@ -21,9 +20,11 @@ class InferenceEngineDetector:
         self.exec_net = self.ie.load_network(network=self.net,
                                         device_name='CPU')
         return
-    def draw_detection(self, img,p1x,p1y,p2x,p2y, line_width,name):
+    def draw_detection(self, img,p1x,p1y,p2x,p2y, line_width,name,fps):
         img = cv2.rectangle(img, (p1x, p1y), (p2x, p2y), (0, 255, 0), line_width)
         img = cv2.putText(img, name, (p1x, p1y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv2.LINE_AA)
+        img = cv2.putText(img, "FPS", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv2.LINE_AA)
+        img = cv2.putText(img, str(fps), (70,70), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv2.LINE_AA)
         return img
     def prepare_image(self, image, h, w):
         image = cv2.resize(image, (w, h))
@@ -32,21 +33,21 @@ class InferenceEngineDetector:
         image = np.expand_dims(image, axis=0)
         return image
     def detect(self, image):
-        input_blob = next(iter(self.net.inputs))
-        out_blob = next(iter(self.net.outputs))
-        n, c, h, w = self.net.inputs[input_blob].shape
-        blob = self.prepare_image(image, h, w)
-        output = self.exec_net.infer(inputs={input_blob: blob})
-        output = output[out_blob]
-        return output
-    def quality(self,image,number_iter):
         times = []
         t0_total = time()
+        number_iter = 1
         for i in range(number_iter):
             t0 = time()
-            output = self.exec_net.infer(...)
+            input_blob = next(iter(self.net.inputs))
+            out_blob = next(iter(self.net.outputs))
+            n, c, h, w = self.net.inputs[input_blob].shape
+            blob = self.prepare_image(image, h, w)
+            output = self.exec_net.infer(inputs={input_blob: blob})
+            output = output[out_blob]
             t1 = time()
             times.append(t1 - t0)
         t1_total = time()
         latency = np.median(times)
-        fps = np.median (number_iter/ (t1_total - t0_total))
+        fps = number_iter / (t1_total - t0_total)
+        return output,fps
+
